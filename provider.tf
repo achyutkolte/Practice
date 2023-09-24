45c8c3de944b238ee0f1440db8d242cf0ebb6b76
achyutkolte <achyut.kolte8@gmail.com>
2023-09-24 19:34:14 +0530

    main branch1

--- provider.tf
+++ provider.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 #This is 1st commit.This is main branch.
-#This is 1st commit.This is feature2 branch.
+#This is 1st commit.This is feature2 branch1.
 
 #This is 1st commit.This is main branch update.
 #This is 1st commit.This is feature1 branch.
