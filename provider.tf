8163215f2ba904025e57f0739755e508562a7ff0
achyutkolte <achyut.kolte8@gmail.com>
2023-09-24 18:28:06 +0530

    main update

--- provider.tf
+++ provider.tf
@@ -2,5 +2,5 @@ provider "aws" {
   region = "us-east-1"           
 }
 
-#This is 1st commit.This is main branch.
+#This is 1st commit.This is main branch update.
 #This is 1st commit.This is feature1 branch.
